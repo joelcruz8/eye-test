3b013bd7fdc9b0694d6278b8da12f27598e14f24
Jonmata <jonna.mata2004@gmail.com>
2024-02-09 22:47:23 -0500

    Update backEnd.py

--- backEnd.py
+++ backEnd.py
@@ -1,20 +1,26 @@
-# import speech_recognition as sr
 
-# # Create a Recognizer instance
-# recognizer = sr.Recognizer()
+import speech_recognition as sr
+import pyttsx3
 
-# # Use the microphone as audio source
-# with sr.Microphone() as source:
-#     print("Speak something...")
-#     audio_data = recognizer.listen(source)
+# Create a Recognizer instance
+recognizer = sr.Recognizer()
+while True: 
+    try:# Use the microphone as audio source
+        with sr.Microphone() as source:
+            print("Speak something...")
+            recognizer.adjust_for_ambient_noise(source, duration=0.2)
+            print("Does this work")
+        
+            audio_data = recognizer.listen(source, phrase_time_limit=1)
+            print("does this work2")
 
-# # Recognize speech using Google Speech Recognition
-# try:
-#     text = recognizer.recognize_google(audio_data)
-#     print("You said: ", text)
-# except sr.UnknownValueError:
-#     print("Google Speech Recognition could not understand the audio")
-# except sr.RequestError as e:
-#     print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    # Recognize speech using Google Speech Recognition
 
-print('Hello world')
+            text = recognizer.recognize_google(audio_data)
+            text=text.lower()
+            print(f"You said:{text} " )
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand the audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
