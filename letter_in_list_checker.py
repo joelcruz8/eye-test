4fc587dffe6fb49e705706ba3013d9ee8d2679f9
joelcruz8 <123519375+joelcruz8@users.noreply.github.com>
2024-02-10 14:35:41 -0500

    Update letter_in_list_checker.py

--- letter_in_list_checker.py
+++ letter_in_list_checker.py
@@ -1,11 +1,9 @@
 import voice_to_text as de
-def check_if_correct(int: i):
+
+def check_if_correct(i: int):
     text= de.receive_input()
     for x in i:
         if x == text:
             return True
      
-    return False   
-       
-
-print(check_if_correct(1))
+    return False
