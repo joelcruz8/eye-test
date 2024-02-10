5499945578fc4f88bf19d24395690b6d6283b9e0
Jonmata <jonna.mata2004@gmail.com>
2024-02-09 23:07:25 -0500

    voice reg
    voice reg

--- backEnd.py
+++ backEnd.py
@@ -1,49 +1,19 @@
-<<<<<<< HEAD
-
 import speech_recognition as sr
 import pyttsx3
 
 # Create a Recognizer instance
 recognizer = sr.Recognizer()
-while True: 
-    try:# Use the microphone as audio source
-        with sr.Microphone() as source:
-            print("Speak something...")
-            recognizer.adjust_for_ambient_noise(source, duration=0.2)
-            print("Does this work")
-        
-            audio_data = recognizer.listen(source, phrase_time_limit=1)
-            print("does this work2")
 
-    # Recognize speech using Google Speech Recognition
-
-            text = recognizer.recognize_google(audio_data)
-            text=text.lower()
-            print(f"You said:{text} " )
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand the audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-=======
-import speech_recognition as sr
-
-# Create a Recognizer instance
-recognizer = sr.Recognizer()
-
-# Use the microphone as audio source
-with sr.Microphone() as source:
-    print("Speak something...")
-    audio_data = recognizer.listen(source)
-
+try:# Use the microphone as audio source
+    with sr.Microphone() as source:
+        print("Speak something...")
+        recognizer.adjust_for_ambient_noise(source, duration=0.2)
+        audio_data = recognizer.listen(source, phrase_time_limit=5)
 # Recognize speech using Google Speech Recognition
-try:
-    text = recognizer.recognize_google(audio_data)
-    print("You said: ", text)
+        text = recognizer.recognize_google(audio_data)
+        text=text.lower()
+        print(f"You said:{text} " )
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand the audio")
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-# print('Hello world')
->>>>>>> ba75d2a6faea9e4da13429e03e6ec35249c933d5
