ba75d2a6faea9e4da13429e03e6ec35249c933d5
joelcruz8 <123519375+joelcruz8@users.noreply.github.com>
2024-02-09 22:27:21 -0500

    added link to website

--- backEnd.py
+++ backEnd.py
@@ -1,20 +1,20 @@
-# import speech_recognition as sr
+import speech_recognition as sr
 
-# # Create a Recognizer instance
-# recognizer = sr.Recognizer()
+# Create a Recognizer instance
+recognizer = sr.Recognizer()
 
-# # Use the microphone as audio source
-# with sr.Microphone() as source:
-#     print("Speak something...")
-#     audio_data = recognizer.listen(source)
+# Use the microphone as audio source
+with sr.Microphone() as source:
+    print("Speak something...")
+    audio_data = recognizer.listen(source)
 
-# # Recognize speech using Google Speech Recognition
-# try:
-#     text = recognizer.recognize_google(audio_data)
-#     print("You said: ", text)
-# except sr.UnknownValueError:
-#     print("Google Speech Recognition could not understand the audio")
-# except sr.RequestError as e:
-#     print("Could not request results from Google Speech Recognition service; {0}".format(e))
+# Recognize speech using Google Speech Recognition
+try:
+    text = recognizer.recognize_google(audio_data)
+    print("You said: ", text)
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand the audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-print('Hello world')
+# print('Hello world')
