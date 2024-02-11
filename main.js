1200a6f23add5484a8f4dc83a842c8c2fe7908d3
Jonmata <jonna.mata2004@gmail.com>
2024-02-10 20:04:14 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+// normel code
+start= true
+counter=0;
+counter_row=0;
+
+while (true){
+    if(start){ 
+        correct_counter=2;
+        start=false;
+    }
+    if(correct_counter>=2){
+        ++counter_row;
+        for(i=1;i>=3;++i){
+
+        }
+
+    }else{
+        if (rightEyeCheck === true) {
+            // end html 
+            
+            break;
+        } else {
+            rightEyeCheck= true;
+            // go to html right eye test
+            break;
+        }
+    }   
+}
